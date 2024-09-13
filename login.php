d7cbdac790e4ae1b663a958a94cc7769f58e5a7b
LarsZauberer <wasser.ian@gmail.com>
2024-09-13 17:47:16 +0200

    Full version

--- login.php
+++ login.php
@@ -5,6 +5,22 @@
     $dbpass = 'admin';
 
     $dbconn = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass");
-    header("Location: http://localhost");
-    die();
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = "SELECT username FROM accounts WHERE username='$username' AND password='$password'";
+    $result = pg_query($query) or die("Error message: " . pg_last_error());
+
+    $found = false;
+    while ($row = pg_fetch_row($result)) {
+        $found = true;
+    }
+
+    if ($found) {
+        echo "You win! Flag found!";
+    } else {
+        header("Location: http://localhost");
+        die();
+    }
 ?>
